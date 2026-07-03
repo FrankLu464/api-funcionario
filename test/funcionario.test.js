63aed7f48e5cf266cc68cc5e1ffa996d13ba76d7
Franklin <franklinluciano464@gmail.com>
2026-07-03 18:00:59 -0300

    Criação de API realizando testes e testando pipeline

---
+++ test/funcionario.test.js
@@ -0,0 +1,19 @@
+const request = require("supertest")
+
+const app = require("../app")
+
+// Escrita de um teste
+describe("API de funcionarios", ()=>{
+    it("Deve cadastrar um funcionario", async()=>{
+        const resposta = await request(app).post("/funcionarios").send({
+            nome: "Maria",
+            cargo: "Analista"
+        })
+
+        expect(resposta.statusCode.tobe(201))
+
+        expect(resposta.body.nome).tobe("Maria")
+
+        expect(resposta.body.cargo).tobe("Analista")
+    })
+})
